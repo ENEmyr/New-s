3c35f151227f4407f3b098dc78a84622983ff467
untesler <keypgphysics@outlook.com>
2020-05-26 12:48:23 +0700

    Add docstring

--- news/News.py
+++ news/News.py
@@ -16,6 +16,21 @@ class News:
         compression_rate:float = .6,
         checkpoints:dict = {}
         ) -> None:
+        """A News class contructor
+
+        Parameters
+        ----------
+        delay : float, optional
+            Delay time in seconds before repete an automatic news scraping system, by default 3600
+        trace_limit : int, optional
+            A number of News to be scrape in each repeat cycle, by default 12
+        summarize_algorithm : str, optional
+            A summarize algorithm name that can select betweet 'text_rank' and 'sentence_rank', by default 'text_rank'
+        compression_rate : float, optional
+            Original News compression rate, by default .6
+        checkpoints : dict, optional
+            A dictionary contain publisher name as a key and list of news ids as a value, by default {}
+        """        
         self.__delay = delay
         self.__trace_limit = trace_limit
         self.__summarize_algorithm = summarize_algorithm
@@ -26,6 +41,13 @@ class News:
         }
     
     def __update_checkpoint(self, latest_news_ids:Dict[str, str]) -> None:
+        """Update current checkpoint, where checkpoint is the list of latest news ids
+
+        Parameters
+        ----------
+        latest_news_ids : Dict[str, str]
+            Latest News ids in previous trace batch
+        """        
         for publisher in latest_news_ids:
             latest_news = []
             if len(latest_news_ids[publisher]) == 0:
@@ -37,7 +59,16 @@ class News:
                     #        self.__checkpoints[publisher].pop()
                     #    self.__checkpoints[publisher].insert(0, news_id)
 
-    def __auto_scrape(self, name=None, run_event=None):
+    def __auto_scrape(self, name=None, run_event=None) -> None:
+        """Automatic scrape news from online news source
+
+        Parameters
+        ----------
+        name : Thread, optional
+            thread name, by default None
+        run_event : Thread, optional
+            run thread event, by default None
+        """        
         print('Scraper worker is starting...')
         api_connector = ApiConnector()
         while run_event.is_set():
@@ -56,10 +87,20 @@ class News:
                         else:
                             print("Raw news pushed.")
                 self.__update_checkpoint(latest_news_ids)
-            print('Sleeping now')
+            print('Scraper is sleeping now...')
             time.sleep(self.__delay)
 
-    def __auto_summarize(self, name=None, run_event=None):
+    def __auto_summarize(self, name=None, run_event=None) -> None:
+        """Automatic summarize scraped news that has been collected in New-sREST api
+
+        Parameters
+        ----------
+        name : Thread, optional
+            thread name, by default None
+        run_event : Thread, optional
+            run thread event, by default None
+
+        """        
         print('Summarize worker is starting...')
         failed_mark_as_summarized = []
         raw_connector = ApiConnector()
@@ -105,11 +146,18 @@ class News:
                         else:
                             print("Re-assign summarizeStatus completed on raw news id {}".format(mark_as_summarized))
                             del(failed_mark_as_summarized[i])
-                time.sleep(self.__delay/2)
+                #time.sleep(self.__delay/2)
             except:
                 print('Some error occur in auto_summarize')
 
-    def start(self):
+    def start(self) -> Dict[list]:
+        """Start automatic news scraping and summarizing system
+
+        Returns
+        -------
+        Dict[list]
+            A dictionary that represented a checkpoint news of each publishers
+        """        
         run_event = threading.Event()
         run_event.set()
         scraper_worker = threading.Thread(target=self.__auto_scrape, args=('scraper', run_event))
